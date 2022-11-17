995b5332835ea11e939fdfaf86fde43a74400eb0
Gabriel Whitehair <gmwhitehair@ksu.edu>
2022-11-17 11:30:05 -0600

    Branch 1

--- ProjectApplication/uxMain.Designer.cs
+++ ProjectApplication/uxMain.Designer.cs
@@ -29,36 +29,164 @@ namespace ProjectApplication
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uxMain));
             this.uxInsertDealButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uxInsertCollegeTownButton = new System.Windows.Forms.Button();
+            this.uxInsertBarButton = new System.Windows.Forms.Button();
+            this.uxInsertVotesButton = new System.Windows.Forms.Button();
+            this.uxInsertCommentButton = new System.Windows.Forms.Button();
+            this.uxInsertUsersButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // uxInsertDealButton
             // 
-            this.uxInsertDealButton.Location = new System.Drawing.Point(69, 126);
+            this.uxInsertDealButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInsertDealButton.ForeColor = System.Drawing.Color.Black;
+            this.uxInsertDealButton.Location = new System.Drawing.Point(57, 123);
             this.uxInsertDealButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.uxInsertDealButton.Name = "uxInsertDealButton";
-            this.uxInsertDealButton.Size = new System.Drawing.Size(142, 58);
+            this.uxInsertDealButton.Size = new System.Drawing.Size(260, 54);
             this.uxInsertDealButton.TabIndex = 0;
             this.uxInsertDealButton.Text = "Insert Deal";
             this.uxInsertDealButton.UseVisualStyleBackColor = true;
             this.uxInsertDealButton.Click += new System.EventHandler(this.uxInsertButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 59);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insert";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // uxInsertCollegeTownButton
+            // 
+            this.uxInsertCollegeTownButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInsertCollegeTownButton.ForeColor = System.Drawing.Color.Black;
+            this.uxInsertCollegeTownButton.Location = new System.Drawing.Point(57, 619);
+            this.uxInsertCollegeTownButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uxInsertCollegeTownButton.Name = "uxInsertCollegeTownButton";
+            this.uxInsertCollegeTownButton.Size = new System.Drawing.Size(260, 54);
+            this.uxInsertCollegeTownButton.TabIndex = 2;
+            this.uxInsertCollegeTownButton.Text = "Insert College Town";
+            this.uxInsertCollegeTownButton.UseVisualStyleBackColor = true;
+            this.uxInsertCollegeTownButton.Click += new System.EventHandler(this.uxInsertCollegeTownButton_Click);
+            // 
+            // uxInsertBarButton
+            // 
+            this.uxInsertBarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInsertBarButton.ForeColor = System.Drawing.Color.Black;
+            this.uxInsertBarButton.Location = new System.Drawing.Point(57, 216);
+            this.uxInsertBarButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uxInsertBarButton.Name = "uxInsertBarButton";
+            this.uxInsertBarButton.Size = new System.Drawing.Size(260, 54);
+            this.uxInsertBarButton.TabIndex = 3;
+            this.uxInsertBarButton.Text = "Insert Bar";
+            this.uxInsertBarButton.UseVisualStyleBackColor = true;
+            this.uxInsertBarButton.Click += new System.EventHandler(this.uxInsertBarButton_Click);
+            // 
+            // uxInsertVotesButton
+            // 
+            this.uxInsertVotesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInsertVotesButton.ForeColor = System.Drawing.Color.Black;
+            this.uxInsertVotesButton.Location = new System.Drawing.Point(57, 521);
+            this.uxInsertVotesButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uxInsertVotesButton.Name = "uxInsertVotesButton";
+            this.uxInsertVotesButton.Size = new System.Drawing.Size(260, 54);
+            this.uxInsertVotesButton.TabIndex = 4;
+            this.uxInsertVotesButton.Text = "Insert Votes";
+            this.uxInsertVotesButton.UseVisualStyleBackColor = true;
+            this.uxInsertVotesButton.Click += new System.EventHandler(this.uxInsertVotesButton_Click);
+            // 
+            // uxInsertCommentButton
+            // 
+            this.uxInsertCommentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInsertCommentButton.ForeColor = System.Drawing.Color.Black;
+            this.uxInsertCommentButton.Location = new System.Drawing.Point(57, 415);
+            this.uxInsertCommentButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uxInsertCommentButton.Name = "uxInsertCommentButton";
+            this.uxInsertCommentButton.Size = new System.Drawing.Size(260, 54);
+            this.uxInsertCommentButton.TabIndex = 5;
+            this.uxInsertCommentButton.Text = "Insert Comment";
+            this.uxInsertCommentButton.UseVisualStyleBackColor = true;
+            this.uxInsertCommentButton.Click += new System.EventHandler(this.uxInsertCommentButton_Click);
+            // 
+            // uxInsertUsersButton
+            // 
+            this.uxInsertUsersButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxInsertUsersButton.ForeColor = System.Drawing.Color.Black;
+            this.uxInsertUsersButton.Location = new System.Drawing.Point(57, 316);
+            this.uxInsertUsersButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uxInsertUsersButton.Name = "uxInsertUsersButton";
+            this.uxInsertUsersButton.Size = new System.Drawing.Size(260, 54);
+            this.uxInsertUsersButton.TabIndex = 6;
+            this.uxInsertUsersButton.Text = "Insert Users";
+            this.uxInsertUsersButton.UseVisualStyleBackColor = true;
+            this.uxInsertUsersButton.Click += new System.EventHandler(this.uxInsertUsersButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(457, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 59);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Update";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(936, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 59);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Query";
+            // 
             // uxMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(1311, 717);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.uxInsertUsersButton);
+            this.Controls.Add(this.uxInsertCommentButton);
+            this.Controls.Add(this.uxInsertVotesButton);
+            this.Controls.Add(this.uxInsertBarButton);
+            this.Controls.Add(this.uxInsertCollegeTownButton);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.uxInsertDealButton);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "uxMain";
-            this.Text = "Main";
+            this.Text = "Manhattan Bar Deals Admin Dashboard";
+            this.Load += new System.EventHandler(this.uxMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button uxInsertDealButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button uxInsertCollegeTownButton;
+        private System.Windows.Forms.Button uxInsertBarButton;
+        private System.Windows.Forms.Button uxInsertVotesButton;
+        private System.Windows.Forms.Button uxInsertCommentButton;
+        private System.Windows.Forms.Button uxInsertUsersButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- ProjectApplication/uxMain.cs
+++ ProjectApplication/uxMain.cs
@@ -20,8 +20,48 @@ namespace ProjectApplication
 
         private void uxInsertButton_Click(object sender, EventArgs e)
         {
-            uxInsertDeal deal = new uxInsertDeal();
-            deal.Show();
+            uxInsertDeal n1 = new uxInsertDeal();
+            n1.Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uxInsertBarButton_Click(object sender, EventArgs e)
+        {
+            uxInsertBar n1 = new uxInsertBar();
+            n1.Show();
+        }
+
+        private void uxInsertUsersButton_Click(object sender, EventArgs e)
+        {
+            uxInsertUser n1 = new uxInsertUser();
+            n1.Show();
+        }
+
+        private void uxInsertCommentButton_Click(object sender, EventArgs e)
+        {
+            uxInsertComment n1 = new uxInsertComment();
+            n1.Show();
+        }
+
+        private void uxInsertVotesButton_Click(object sender, EventArgs e)
+        {
+            uxInsertVotes n1 = new uxInsertVotes();
+            n1.Show();
+        }
+
+        private void uxInsertCollegeTownButton_Click(object sender, EventArgs e)
+        {
+            uxInsertCollegeTown n1 = new uxInsertCollegeTown();
+            n1.Show();
         }
     }
 }
